3f05cac725b9b11a04f03cd1e07b4055f2e12922
Kevin-Kawai <kevin.fredricks@gmail.com>
2018-02-06 21:41:07 +0900

    implemented Curriculum create and show

--- app/controllers/curriculums_controller.rb
+++ app/controllers/curriculums_controller.rb
@@ -6,4 +6,25 @@ class CurriculumsController < ApplicationController
   def new
     @curriculum = Curriculum.new
   end
+
+  def create
+
+    @curriculum = current_user.curriculums.new(curriculum_params)
+
+    if @curriculum.save!
+      redirect_to curriculums_path
+    else
+      render status: 500
+    end
+  end
+
+  def show
+    @curriculum = Curriculum.find(params[:id])
+  end
+
+  private
+
+  def curriculum_params
+    params.require(:curriculum).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pages#index"
-  resources "curriculums", only: [:index, :new]
+  resources "curriculums", only: [:index, :new, :create, :show]
   get "lesson", to: "lessons#index"
 end

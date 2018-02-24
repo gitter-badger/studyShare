290a93f2aee36f85ae7909766fe032bf869adaea
Kevin-Kawai <kevin.fredricks@gmail.com>
2018-02-24 19:51:40 +0900

    implemented delete for lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,5 @@
 class LessonsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def show
     @lesson = Lesson.find(params[:id])
@@ -19,6 +19,27 @@ class LessonsController < ApplicationController
     else
       render status: 500
     end
+  end
+
+  def edit
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+    @lesson.update_attributes (lesson_params)
+    redirect_to curriculum_lesson_path(@lesson, curriculum_id: @lesson.curriculum.id)
+  end
+
+  def destroy
+    @lesson = Lesson.find(params[:id])
+
+    if @lesson.curriculum.user != current_user
+      return render text: "Not Allowed", status: :forbidden
+    end
+
+    @lesson.destroy
+    redirect_to curriculum_path(@lesson.curriculum)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pages#index"
   resources "curriculums" do
-    resources "lessons", only: [:show, :create, :new]
+    resources "lessons"
   end
 end
